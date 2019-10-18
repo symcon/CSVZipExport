8af1406a835b8621dda6752b406b9609cb5b5b7e
Michael Maroszek <paresy@gmail.com>
2019-10-18 10:50:19 +0200

    fix style

--- CSVZipExport/module.php
+++ CSVZipExport/module.php
@@ -82,7 +82,8 @@ class CSVZipExport extends IPSModule
     public function DeleteZip()
     {
         $tempfile = IPS_GetKernelDir() . 'webfront' . DIRECTORY_SEPARATOR . 'user' . DIRECTORY_SEPARATOR . 'CSV_' . $this->InstanceID . '.zip';
-        if(file_exists($tempfile)) {
+        if(file_exists($tempfile))
+        {
             unlink($tempfile);
         }    
         $this->SetTimerInterval('DeleteZipTimer', 0);
